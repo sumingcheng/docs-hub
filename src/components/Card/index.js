733c0fbd5defcea882ed2d486f6fbc65cf686f5c
sumingcheng <cjwdyzdyplxjg@gmail.com>
2024-12-11 15:23:03 +0800

    增加_category_.json，修改go目录顺序

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import Link from '@docusaurus/Link'
-import './styles.scss'
+import styles from './styles.module.scss'
 
 export function Card({ title, description, to }) {
   return (
-    <Link to={to} className="card">
-      <div className="card-content">
-        <h3 className="card-title">{title}</h3>
-        <p className="card-description">{description}</p>
+    <Link to={to} className={styles.card}>
+      <div className={styles.cardContent}>
+        <h3 className={styles.cardTitle}>{title}</h3>
+        <p className={styles.cardDescription}>{description}</p>
       </div>
     </Link>
   )
@@ -15,7 +15,7 @@ export function Card({ title, description, to }) {
 
 export function CardGrid({ children }) {
   return (
-    <div className="row">
+    <div className={styles.row}>
       {children}
     </div>
   )
