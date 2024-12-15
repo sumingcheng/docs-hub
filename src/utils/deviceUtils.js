b0bdd120dff626f9b988853e4fa48fc6c10e4973
sumingcheng <cjwdyzdyplxjg@gmail.com>
2024-12-15 19:19:53 +0800

    更新 hooks

--- src/utils/deviceUtils.js
+++ src/utils/deviceUtils.js
@@ -1,19 +1,28 @@
 import React from 'react'
+import { throttle } from 'lodash'
+
 export const isMobile = () => {
   if (typeof window === 'undefined') return false
   return window.innerWidth <= 768
 }
 
 export const useIsMobile = () => {
-  const [isMobileView, setIsMobileView] = React.useState(isMobile())
+  const [isMobileView, setIsMobileView] = React.useState(false)
 
   React.useEffect(() => {
-    const handleResize = () => {
+    setIsMobileView(isMobile())
+    
+    const handleResize = throttle(() => {
       setIsMobileView(isMobile())
-    }
+    }, 200)
 
     window.addEventListener('resize', handleResize)
-    return () => window.removeEventListener('resize', handleResize)
+    window.addEventListener('orientationchange', handleResize)
+
+    return () => {
+      window.removeEventListener('resize', handleResize)
+      window.removeEventListener('orientationchange', handleResize)
+    }
   }, [])
 
   return isMobileView
